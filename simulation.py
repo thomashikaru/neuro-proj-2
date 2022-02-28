efc51301caf03fb8946c7ad23685a1f634cb3e1b
Thomas Clark <thclark@mit.edu>
2022-02-27 19:00:21 -0500

    eta value sweep

--- simulation.py
+++ simulation.py
@@ -1,6 +1,13 @@
 import numpy as np
 import collections
+from itertools import chain, repeat, count, cycle, islice, product
 import utils
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.signal import find_peaks
+
+sns.set_style("dark")
 
 
 class RLModel:
@@ -67,17 +74,93 @@ class RLModel:
         )
 
 
+def choice_experiment():
+    high_HPS_win_WOF = RLModel(f=1.6, eta_h=0.28, eta_v=0.15, suffix="high_HPS_win_WOF")
+    low_HPS_win_WOF = RLModel(f=0.85, eta_h=0.28, eta_v=0.15, suffix="low_HPS_win_WOF")
+    high_HPS_lose_WOF = RLModel(
+        f=1.6, eta_h=0.28, eta_v=0.15, suffix="high_HPS_lose_WOF"
+    )
+    low_HPS_lose_WOF = RLModel(
+        f=0.85, eta_h=0.28, eta_v=0.15, suffix="low_HPS_lose_WOF"
+    )
+
+    # rewards_win_WOF = chain(repeat(10, 10), repeat(20, 5), repeat(10, 10))
+    # rewards_lose_WOF = chain(repeat(10, 10), repeat(0, 5), repeat(10, 10))
+
+    rewards_win_WOF = islice(cycle(range(10)), 100)
+    rewards_lose_WOF = islice(cycle(list(range(10))[::-1]), 100)
+
+    for r in rewards_win_WOF:
+        high_HPS_win_WOF.step(r)
+        low_HPS_win_WOF.step(r)
+
+    for r in rewards_lose_WOF:
+        high_HPS_lose_WOF.step(r)
+        low_HPS_lose_WOF.step(r)
+
+    models = [high_HPS_win_WOF, low_HPS_win_WOF, high_HPS_lose_WOF, low_HPS_lose_WOF]
+
+    dfs = [
+        pd.DataFrame(
+            {
+                "Condition": m.suffix,
+                "Iteration": range(len(m.history["expected value"])),
+                "Value": m.history["expected value"],
+            }
+        )
+        for m in models
+    ]
+    df = pd.concat(dfs)
+
+    plot = sns.lineplot(data=df, x="Iteration", y="Value", hue="Condition")
+    plt.savefig("img/choice_experiment.png", dpi=300)
+
+
+def eta_sweep():
+    eta_hs = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65]
+    eta_vs = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65]
+
+    data = []
+
+    for eta_h, eta_v in product(eta_hs, eta_vs):
+        model = RLModel(f=1.6, eta_h=eta_h, eta_v=eta_v, suffix=f"{eta_h}_{eta_v}")
+        model.simulate(300, reward=10)
+        moods = model.history["mood"]
+        peak_idxs, _ = find_peaks(moods, distance=5)
+        if len(peak_idxs) != 0:
+            period = len(moods) / len(peak_idxs)
+        else:
+            period = np.nan
+        freq = 1.0 / period
+        data.append({"eta_h": eta_h, "eta_v": eta_v, "period": period, "freq": freq})
+
+    df = pd.DataFrame(data)
+    df = df.pivot(index="eta_h", columns="eta_v", values="freq")
+
+    sns.heatmap(data=df, cmap="viridis")
+    plt.savefig("img/eta_sweep.png", dpi=300)
+
 
 if __name__ == "__main__":
 
-    fs = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
-    suffixes = [str(x).replace(".", "-") for x in fs]
-    histories = []
-    for f, suffix in zip(fs, suffixes):
-        model = RLModel(f=f, eta_h=0.1, eta_v=0.1, suffix=suffix)
-        histories.append(model.simulate(iters=500, reward=10))
-        # model.plot()
-    utils.save_parameter_lineplot(histories, fs)
+    # fs = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5]
+    # suffixes = [str(x).replace(".", "-") for x in fs]
+    # histories = []
+    # for f, suffix in zip(fs, suffixes):
+    #     model = RLModel(f=f, eta_h=0.1, eta_v=0.1, suffix=suffix)
+    #     histories.append(model.simulate(iters=500, reward=10))
+    #     # model.plot()
+    # utils.save_parameter_lineplot(histories, fs)
 
+    # eta_hs = [0.01, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30]
+    # suffixes = [str(x).replace(".", "-") for x in eta_hs]
+    # histories = []
+    # for eta, suffix in zip(eta_hs, suffixes):
+    #     model = RLModel(f=1.6, eta_h=eta, eta_v=0.1, suffix=suffix)
+    #     histories.append(model.simulate(iters=500, reward=10))
+    #     # model.plot()
+    # utils.save_parameter_lineplot(histories, eta_hs)
 
+    # choice_experiment()
 
+    eta_sweep()
